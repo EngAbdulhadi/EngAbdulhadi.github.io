0412398623e79bea1cf7a5b46748d0f7e1ae1afb
EngAbdulhadi <abdulhadikremesh@gmail.com>
2025-04-30 22:17:55 +0300

    edit php site

--- login.php
+++ login.php
@@ -1,16 +1,24 @@
 <?php
 
-if (isset($_POST['E-Posta'], $_POST['sifre']) 
-    && $_POST['E-Posta'] != '' 
-    && $_POST['sifre'] != '') {
+$email = $_POST['E-Posta'] ?? '';
+$password = $_POST['sifre'] ?? '';
+
+
+$valid_email = 'abdulhadi.krimesh@ogr.sakarya.edu.tr';
+$valid_password = 'G231210577';
+
+if (!empty($email) && !empty($password)) {
     
-    echo "Kayıt İşleminiz Yapıldı...<br>";
-    echo "<b>" . $_POST['E-Posta'] . ' - ' . $_POST['sifre'] . "</b>";
-    
+    if ($email === $valid_email && $password === $valid_password) {
+        $username = explode('@', $email)[0];
+        echo "<b>Hoşgeldiniz $username</b><br>";
+    } else {
+        echo "Bilgileriniz Hatalı. Lütfen tekrar deneyin.<br>";
+        echo "<p><a href='html/login.html'>&lt;GERİ DÖN&gt;</a></p>";
+    }
+
 } else {
-    echo "Bilgilerinizi Kontrol Edip Tekrar Giriş Yapın.<br>";
-    echo "Eksik Bilgi Girdiniz...<br>";
+    echo "Eksik bilgi girdiniz. Lütfen tüm alanları doldurun.<br>";
     echo "<p><a href='html/login.html'>&lt;GERİ DÖN&gt;</a></p>";
 }
-
 ?>
