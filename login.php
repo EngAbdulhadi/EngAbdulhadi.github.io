3410661e558c4709805a9544b3f018eef4e86d72
EngAbdulhadi <abdulhadikremesh@gmail.com>
2025-05-15 04:24:06 +0300

    new edits

--- login.php
+++ login.php
@@ -12,6 +12,7 @@ if (!empty($email) && !empty($password)) {
     if ($email === $valid_email && $password === $valid_password) {
         $username = explode('@', $email)[0];
         echo "<b>Hoşgeldiniz $username</b><br>";
+        echo "<p><a href='index.html'>&lt;Hakkimda sayfasina git&gt;</a></p>";
     } else {
         echo "Bilgileriniz Hatalı. Lütfen tekrar deneyin.<br>";
         echo "<p><a href='html/login.html'>&lt;GERİ DÖN&gt;</a></p>";
